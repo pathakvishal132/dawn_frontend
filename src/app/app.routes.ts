926fd12b77d453b6e13fb848874abe203cf324fc
Himanshu Pathak <pathakvishal132@gmail.com>
2026-06-21 23:44:51 +0530

    fixed routing

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,6 @@
 import { Routes } from '@angular/router';
-import { DashboardComponent } from './dashboard/dashboard.component';
 import { TravelComponent } from './travel/travel.component';
 
 export const routes: Routes = [
-  { path: '', redirectTo: '/travel', pathMatch: 'full' },
-  { path: 'dashboard', component: DashboardComponent },
-  { path: 'travel', component: TravelComponent },
+  { path: '', component: TravelComponent },
 ];
